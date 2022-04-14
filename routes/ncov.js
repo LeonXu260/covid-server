8ba15e910fac2ba3d52acf3d3fdb6f61af26c38d
Leon Xu <dcwinfield362@gmail.com>
2022-04-13 20:13:53 -0700

    doc: add image route

--- routes/ncov.js
+++ routes/ncov.js
@@ -5,6 +5,7 @@ import {
   getChinaData,
   getAbroadData,
   ChinaData,
+  getImage,
   WorldData,
   TravelDataCity,
 } from "../controller/ncov.js";
@@ -15,6 +16,7 @@ router.get("/", getChinaData);
 router.get("/abroad", getAbroadData);
 router.get("/china", ChinaData);
 router.get("/world", WorldData);
+router.get("/image", getImage);
 router.get("/travel", TravelDataCity);
 
 export default router;
